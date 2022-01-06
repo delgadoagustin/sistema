651a8000138a78ec54c90cd3e28b6393002a411b
delgadoagustin <delgadoagustin147@gmail.com>
2022-01-05 21:51:32 -0300

    Add Entidades

---
+++ Sistema/Sistema/Models/Entidades/Medico.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sistema.Models.Entidades
+{
+    public class Medico
+    {
+        int id;
+        string nombre;
+        string matricula;
+
+        public int Id { get => id; set => id = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Matricula { get => matricula; set => matricula = value; }
+    }
+}
---
+++ Sistema/Sistema/Models/Entidades/Paciente.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sistema.Models.Entidades
+{
+    public class Paciente
+    {
+        int id;
+        string nombre;
+        string email;
+        string telefono;
+        string dni;
+        char sexo;
+        DateTime fechaNac;
+
+        public int Id { get => id; set => id = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Email { get => email; set => email = value; }
+        public string Telefono { get => telefono; set => telefono = value; }
+        public string Dni { get => dni; set => dni = value; }
+        public char Sexo { get => sexo; set => sexo = value; }
+        public DateTime FechaNac { get => fechaNac; set => fechaNac = value; }
+    }
+}
